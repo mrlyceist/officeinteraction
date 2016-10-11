039a8583328ef659b4b52323fe302650c993bdab
mrlyceist <dorogov.alexey@gmail.com>
2016-10-11 21:40:53 +0500

    temporary commit for transfer

---
+++ excelinteraction/ExcelDocument.cs
@@ -0,0 +1,27 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelInteraction
+{
+    public class ExcelDocument
+    {
+        public void CreatePackage(string filePath)
+        {
+            using (SpreadsheetDocument package = SpreadsheetDocument.Create(filePath, SpreadsheetDocumentType.Workbook))
+            {
+                CreateParts(package);
+            }
+        }
+
+
+        public void Save(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
