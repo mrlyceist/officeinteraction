b4a6a8107914ae2197168e0b3122fe99c0a47a06
leshka <adorogov@ooobit.com>
2016-10-11 21:40:53 +0500

    temporary commit for transfer

---
+++ excelinteractiontests/ExcelDocumentTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace ExcelInteractionTests
+{
+    [TestClass]
+    public class ExcelDocumentTests
+    {
+        [TestMethod]
+        public void BlankExcelDocumentIsCreated()
+        {
+            var xlDoc = new ExcelInteraction.ExcelDocument();
+            xlDoc.Save("test.xlsx");
+
+            Assert.IsTrue(File.Exists("test.xlsx"));
+        }
+    }
+}
