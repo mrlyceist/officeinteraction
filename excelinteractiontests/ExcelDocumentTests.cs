e85d7f8d65f22917c58477fa355a99f4478a0669
leshka <adorogov@ooobit.com>
2016-10-24 20:48:04 +0500

    Now we can save text to cell in Excel.

--- excelinteractiontests/ExcelDocumentTests.cs
+++ excelinteractiontests/ExcelDocumentTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
 
 namespace ExcelInteractionTests
@@ -10,10 +9,26 @@ namespace ExcelInteractionTests
         [TestMethod]
         public void BlankExcelDocumentIsCreated()
         {
-            var xlDoc = new ExcelInteraction.ExcelDocument();
-            xlDoc.Save("test.xlsx");
+            var filename = "test.xlsx";
+            var xlDoc = new ExcelInteraction.ExcelDocument(filename);
+            //xlDoc.Save(filename);
 
-            Assert.IsTrue(File.Exists("test.xlsx"));
+            Assert.IsTrue(File.Exists(filename));
+        }
+
+        [TestMethod]
+        public void StringValueIsAddedToTheFirstCellInFirstRow()
+        {
+            var fileName = "test.xlsx";
+            var xlDoc = new ExcelInteraction.ExcelDocument(fileName);
+            //xlDoc.InsertText()
+            ExcelInteraction.ExcelDocument.InsertText(fileName, "test");
+
+            var dataTable = NCore.General.GetTableFromExcel(fileName);
+
+            string test = dataTable.Rows[0][0].ToString();
+
+            Assert.AreEqual("test", test);
         }
     }
 }
