fbe91ceb6b1a38a4af832d3d9e3906925dcd85f8
leshka <adorogov@ooobit.com>
2016-11-02 00:16:30 +0500

    Possible fix of *CellIsNull* bug.

--- excelinteraction/Properties/AssemblyInfo.cs
+++ excelinteraction/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
-[assembly: AssemblyVersion("1.1.12.25")]
-[assembly: AssemblyFileVersion("1.1.12.25")]
+[assembly: AssemblyVersion("1.1.13.26")]
+[assembly: AssemblyFileVersion("1.1.13.26")]
